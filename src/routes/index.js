3ea3101fa752e544a9a2ee5b0133cd2f46007aa8
Quang Minh <minhdq200902@gmail.com>
2024-06-13 03:31:05 +0700

    Update layout and header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import Home from '~/page/Home';
 import Login from '~/page/Login';
+import User from '~/page/User';
+import user from '~/page/User';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/login', component: Login },
 ];
 
-const privateRoutes = [];
+const privateRoutes = [{ path: '/user', component: User }];
 
 export { publicRoutes, privateRoutes };
